48b729643eb100fb072cfc4a8271633398b04c71
Code <gsarciotto@gmail.com>
2020-09-24 11:05:03 -0300

    Added dependency: uuid. Created wrapper

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,1 +1,7 @@
 export { IDatabase, Database } from "./wrappers/slonik";
+
+export { IJwt, Jwt } from "./wrappers/jwt";
+
+export { IUuid, Uuid } from "./wrappers/uuid";
+
+export { IBcrypt, Bcrypt } from "./wrappers/bcrypt";
