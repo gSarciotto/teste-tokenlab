f56529cb1d81ce3aee9b53f0fcbfb5c7928b5552
Code <gsarciotto@gmail.com>
2020-09-25 13:45:47 -0300

    Added authorization utils

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -5,3 +5,13 @@ export { IJwt, Jwt } from "./wrappers/jwt";
 export { IUuid, Uuid } from "./wrappers/uuid";
 
 export { IBcrypt, Bcrypt } from "./wrappers/bcrypt";
+
+export {
+    InvalidAuthorizationHeader,
+    getTokenPayloadFromAuthorizationHeader
+} from "./authorization/authorization";
+
+export {
+    AuthorizationDatabase,
+    IAuthorizationDatabase
+} from "./authorization/AuthorizationDatabase";
