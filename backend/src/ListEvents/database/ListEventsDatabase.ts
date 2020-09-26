e96790a21d030313e63fe056cb08bbaf6cd88ccb
Code <gsarciotto@gmail.com>
2020-09-26 14:16:31 -0300

    Implemented ListEventsDatabase

--- backend/src/ListEvents/database/ListEventsDatabase.ts
+++ backend/src/ListEvents/database/ListEventsDatabase.ts
@@ -1,7 +1,8 @@
-import { IDatabase, convertEventModelToEvent } from "../../utils";
-import { EventModel } from "../../sharedResources";
+import { sql, NotFoundError } from "slonik";
+import { IDatabase } from "../../utils";
 import { EventWithUsername } from "../EventWithUsername";
 import { EventWithUsernameModel } from "./EventWithUsernameModel";
+import { convertFromModel } from "./convertFromModel";
 
 export interface IListEventsDatabase {
     getEvents: (ownerId: string) => Promise<EventWithUsername[]>;
@@ -9,5 +10,21 @@ export interface IListEventsDatabase {
 
 export class ListEventsDatabase implements IListEventsDatabase {
     constructor(private database: IDatabase) {}
-    getEvents(ownerId: string): Promise<EventWithUsername[]> {}
+    async getEvents(creatorId: string): Promise<EventWithUsername[]> {
+        let queryResults: EventWithUsernameModel[];
+        try {
+            const joinQuery = sql`WITH users_and_events AS (SELECT events.id AS id, users.username, events.creator_id, events.begin_time, events.end_time FROM events INNER JOIN users ON users.id=events.creator_id WHERE users.id=${creatorId})`;
+            queryResults = await this.database.pool.many(
+                sql`${joinQuery} SELECT id, begin_time, end_time, username AS creator_username FROM users_and_events ORDER BY begin_time DESC`
+            );
+        } catch (err) {
+            if (err instanceof NotFoundError) {
+                queryResults = [];
+            } else {
+                throw err;
+            }
+        }
+        const convertedResults = queryResults.map(convertFromModel);
+        return convertedResults;
+    }
 }
