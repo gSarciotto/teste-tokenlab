3c56fb6c55e50e91789a15eb15f3bc76fe902816
Code <gsarciotto@gmail.com>
2020-09-26 18:12:50 -0300

    Implemented UpdateEventDatabase

--- backend/src/UpdateEvent/database/UpdateEventDatabase.ts
+++ backend/src/UpdateEvent/database/UpdateEventDatabase.ts
@@ -1,17 +1,46 @@
+import {
+    sql,
+    SqlSqlTokenType,
+    QueryResultRowType,
+    NotFoundError
+} from "slonik";
 import { IDatabase } from "../../utils";
-import { UpdateEventBody } from "../route";
+import { ConvertedUpdateEventBody } from "../route";
+import { NotFound } from "./errors";
 
 export interface IUpdateEventDatabase {
     updateEvent: (
-        eventInformation: UpdateEventBody,
+        eventInformation: ConvertedUpdateEventBody,
         creatorId: string
     ) => Promise<void>;
 }
 
+const dateParam = (
+    dateObj: Date
+): SqlSqlTokenType<QueryResultRowType<string>> => {
+    return sql`TO_TIMESTAMP(${dateObj.getTime()} / 1000.0)`;
+};
+
 export class UpdateEventDatabase implements IUpdateEventDatabase {
     constructor(private database: IDatabase) {}
     async updateEvent(
-        eventInformation: UpdateEventBody,
+        eventInformation: ConvertedUpdateEventBody,
         creatorId: string
-    ): Promise<void> {}
+    ): Promise<void> {
+        try {
+            await this.database.pool.one(
+                sql`UPDATE events SET begin_time=${dateParam(
+                    eventInformation.begin
+                )}, end_time=${dateParam(eventInformation.end)} WHERE id=${
+                    eventInformation.id
+                } AND creator_id=${creatorId} RETURNING *`
+            );
+        } catch (err) {
+            if (err instanceof NotFoundError) {
+                throw new NotFound();
+            } else {
+                throw err;
+            }
+        }
+    }
 }
