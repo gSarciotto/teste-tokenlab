60583c447d4cefee78aeae53c3f96d88f213d96c
Code <gsarciotto@gmail.com>
2020-09-28 09:50:35 -0300

    Created login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { LoginAndRegisterPage } from "./pages/LoginAndRegister";
 import "./App.css";
 
+const useStyles = makeStyles({
+    globalContainer: (theme: Theme) => ({
+        height: "100vh",
+        width: "100vw",
+        padding: "10% 0",
+        backgroundColor: theme.palette.primary.main
+    })
+});
+
 function App(): JSX.Element {
-    return <div className="App"></div>;
+    const theme = useTheme();
+    const classes = useStyles(theme);
+    const [userToken, setUserToken] = useState("");
+    const changeUserToken = (newToken: string): void => {
+        setUserToken(newToken);
+    };
+    return (
+        <Grid
+            className={classes.globalContainer}
+            container
+            justify="center"
+            alignItems="center"
+            color="primary"
+        >
+            <LoginAndRegisterPage updateAuthenticatedUser={changeUserToken} />
+        </Grid>
+    );
 }
 
 export default App;
